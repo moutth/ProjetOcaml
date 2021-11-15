b9a916c738b285922ab0996454b4b4cb8ea980c5
Matthias <75242607+moutth@users.noreply.github.com>
2021-11-15 16:34:49 +0100

    Update tools.ml

--- src/tools.ml
+++ src/tools.ml
@@ -4,4 +4,9 @@ open Graph
 (* assert false is of type ∀α.α, so the type-checker is happy. *)
 let clone_nodes gr = n_fold gr new_node empty_graph
 let gmap gr f = assert false
-let add_arc g id1 id2 n= assert false
+let add_arc g id1 id2 n =
+  let lbl = find_arc g id1 id2 in
+  match lbl with
+  | Some(x) -> new_arc g id1 id2 (x + n)
+  | None -> new_arc g id1 id2 n
+  
