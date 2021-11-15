60e69275d5451cb1ea95190b595803b1c34aa772
Matthias <75242607+moutth@users.noreply.github.com>
2021-11-15 15:45:13 +0100

    Delete tools.ml

--- tools.ml
+++
@@ -1,7 +0,0 @@
-(* Yes, we have to repeat open Graph. *)
-open Graph
-
-(* assert false is of type ∀α.α, so the type-checker is happy. *)
-let clone_nodes gr = n_iter (new_node gr)
-let gmap gr f = assert false
-let add_arc g id1 id2 n= assert false
