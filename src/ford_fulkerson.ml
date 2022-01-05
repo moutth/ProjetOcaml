d048d056fe39053ba29dda06c53abaf2b36ef118
Riffard Matthias <riffard@etud.insa-toulouse.fr>
2022-01-05 19:12:06 +0100

    Projet corrigé et terminé ! :)

--- src/ford_fulkerson.ml
+++ src/ford_fulkerson.ml
@@ -4,38 +4,42 @@ open Tools
 
 let find_min graph path = 
   match path with
-  | [] -> failwith "ça marche pas c'est pas normal"
-  | [hd] -> failwith "fais un effort mec"
+  | [] -> failwith "recherche de min sur chemin vide"
+  | [hd] -> failwith "recherche de min sur chemin d'un seul noeud"
   | hd::(nxt::tl) -> 
     let rec parcours prev reste min = 
       match reste with
       | [] -> min
       | snd::suite -> 
         let lbl = find_arc graph prev snd in
-        Printf.printf("prev=%d scd=%d min=%d%!\n") prev snd min ;
         match lbl with
-        | None -> failwith "biip"
+        | None -> failwith "arc sans label lors de recherche de min"
         | Some s -> let res = s in
+          Printf.printf "prev=%d scd=%d min=%d res=%d%!\n" prev snd min res;
           if res > min then parcours snd suite min else parcours snd suite res
     in match find_arc graph hd nxt with
-    | None -> failwith "biiiiiiip"
+    | None -> failwith "arc inexistant dans le chemin donné"
     | Some x -> parcours hd (nxt::tl) x 
 
 let update_graph graph path min =
   match path with
-  | [] -> failwith "ça marche pas c'est pas normal"
-  | [hd] -> failwith "fais un effort mec"
+  | [] -> failwith "mise à jour du graph impossible : chemin vide"
+  | [hd] -> failwith "mise à jour du graph impossible : chemin d'un seul noeud"
   | hd::(nxt::tl) -> 
     let rec parcours gr prev reste = 
       match reste with
       | [] -> gr
-      | snd::suite -> parcours ( add_arc (add_arc gr prev snd (-min)) snd prev min) snd suite
+      | snd::suite -> parcours (add_arc (add_arc gr prev snd (-min)) snd prev min) snd suite
     in parcours graph hd (nxt::tl)
 
 
 let rec ff20 graph source target max_flow =
   let path = find_path graph source target in
-  let min = find_min graph path in
-  match min with 
-  | 0 -> max_flow
-  | x -> ff20 (update_graph graph path x) source target (max_flow+min)
+  if not (List.mem target path) then
+    max_flow (* on s'arrete si aucun chemin nous reliant à la target n'existe *)
+  else
+    let min = find_min graph path in
+    print_string (String.concat " " (List.map string_of_int path)); Printf.printf " min = %d%!\n" min;
+    match min with 
+    | 0 -> max_flow
+    | x -> ff20 (update_graph graph path x) source target (max_flow+min)
--- src/parcours.ml
+++ src/parcours.ml
@@ -2,19 +2,21 @@ open Graph
 
 
 let find_path graph src target =
-  if not (node_exists graph src && node_exists graph target) then raise (Graph_error ("A node does not exist in the graph."))
+  if not (node_exists graph src && node_exists graph target) then
+    raise (Graph_error ("A node does not exist in the graph."))
   else
     let rec explore current_node path out_list visited =
       match out_list with
       | [] -> []
-      | (next_node,_)::reste -> if (List.mem next_node visited) || ((find_arc graph current_node next_node) == Some(0))  then explore current_node path reste (List.append [current_node;next_node] visited)
-        else
-        if next_node = target
-        then List.append [next_node;current_node] path 
+      | (next_node,_)::reste -> 
+        if (List.mem next_node visited) || ((find_arc graph current_node next_node) <= Some(0)) then
+          explore current_node path reste visited
+        else if next_node = target then
+          List.append [next_node;current_node] path
         else
           let chemin = explore next_node (current_node::path) (out_arcs graph next_node) (current_node::visited) in
-          if chemin <> []
-          then chemin
-          else 
+          if chemin <> [] then
+            chemin
+          else
             explore current_node path reste (List.append [current_node;next_node] visited)
     in List.rev (explore src [] (out_arcs graph src) [])
